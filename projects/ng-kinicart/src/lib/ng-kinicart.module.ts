4c08a2e2cb7ff98ecd524b94d6f9c91cd47835ac
Nathan Alan <nathan@oxil.co.uk>
2019-04-11 11:57:26 +0100

    ng-kinicart library created with first component

---
+++ projects/ng-kinicart/src/lib/ng-kinicart.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+    declarations: [AuthComponent],
+    imports: [
+
+    ],
+    exports: [
+        AuthComponent
+    ]
+})
+export class NgKinicartModule {
+}
---
+++ projects/ng-kinicart/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-kinicart
+ */
+
+export * from './lib/ng-kinicart.module';
+export * from './lib/auth/auth.component';
