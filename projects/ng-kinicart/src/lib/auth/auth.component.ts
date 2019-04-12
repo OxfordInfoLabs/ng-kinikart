71d70b6de11a793e9d605534d5f3040985225e36
Nathan Alan <nathan@oxil.co.uk>
2019-04-12 12:16:30 +0100

    updated auth component

--- projects/ng-kinicart/src/lib/auth/auth.component.ts
+++ projects/ng-kinicart/src/lib/auth/auth.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { KinibindModel, KinibindRequestService } from "ng-kinibind";
+import { Subject } from "rxjs/internal/Subject";
 
 @Component({
-  selector: 'kc-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.sass']
+    selector: 'kc-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.sass']
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+    @Input() authService;
 
-  ngOnInit() {
-  }
+    public security: KinibindModel = new KinibindModel({});
+    public twoFactorConfig: KinibindModel = new KinibindModel({});
+    public reloadTwoFactor: Subject<boolean> = new Subject();
+    public isLoading = true;
+
+    constructor(private kbRequest: KinibindRequestService) {
+    }
+
+    ngOnInit() {
+
+    }
+
+    public resetAccountPassword() {
+        const message = 'We are going to reset the account password to a temporary one and email the registered ' +
+            'email address. Are you sure you wish to proceed?';
+        if (window.confirm(message)) {
+            this.authService.resetAccountPassword();
+        }
+    }
+
+    public disable2FA() {
+        const message = 'Are you sure you would like to turn off Two Factor Authentication?';
+        if (window.confirm(message)) {
+            this.kbRequest.makeGetRequest('/internal/account/disableGoogle2FA')
+                .toPromise()
+                .then(() => {
+                    this.reloadTwoFactor.next(true);
+                });
+        }
+    }
+
 
 }
--- projects/ng-kinicart/src/lib/ng-kinicart.module.ts
+++ projects/ng-kinicart/src/lib/ng-kinicart.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
+import { NgKinibindModule } from 'ng-kinibind';
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { MatProgressSpinnerModule } from "@angular/material";
 
 @NgModule({
     declarations: [AuthComponent],
     imports: [
-
+        NgKinibindModule,
+        RouterModule,
+        CommonModule,
+        MatProgressSpinnerModule
     ],
     exports: [
         AuthComponent
+    ],
+    providers: [
     ]
 })
 export class NgKinicartModule {
